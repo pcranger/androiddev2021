ff269292254568fcbf5449779fc7e84c8a84a3de
pcranger <52710878+pcranger@users.noreply.github.com>
2020-12-11 15:04:56 +0700

    this project has reached practical work 5 so far

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/Fragment2.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/Fragment2.java
@@ -13,7 +13,7 @@ public class Fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragment 
         return inflater.inflate(R.layout.fragment_2, container, false);
     }
 }
